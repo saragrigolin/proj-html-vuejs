25fe3f4bfe1fd09edda48f4b2ffbfba9904c64b9
saragrigolin <sara.grigolin95@gmail.com>
2022-01-24 10:49:19 +0100

    scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header></header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/style.scss";
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <main></main>
+</template>
+
+<script>
+export default {
+    name: "Main",
+
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/style.scss";
+
+</style>
