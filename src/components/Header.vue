a5d845e03fc743e300bce106990c4a1e6236336b
saragrigolin <sara.grigolin95@gmail.com>
2022-01-25 09:51:27 +0100

    edit header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,26 @@
 <template>
     <header>
-        <div class="container-fluid p-0 position-relative">
-            <div class="navbar-container position-absolute">
+        <div class="container-fluid my_fluid p-0 position-relative">
+            <div class="navbar-container position-fixed">
                 <nav class="navbar navbar-dark px-3">
                     <a class="navbar-brand my_navbar-brand" href="#">
                         <img src="../assets/img/avada-music-logo.png" alt="">
                     </a>
-                    <button class="navbar-toggler my_navbar-toggler" @click="getBackground()" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler my_navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon open"></span>
                         <i class="close fas fa-times"></i>
                     </button>
                     <div class="collapse navbar-collapse my_collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="#">Home</a>
-                        <a class="nav-item nav-link" href="#">Meet the brand</a>
-                        <a class="nav-item nav-link" href="#">Live dates</a>
-                        <a class="nav-item nav-link" href="#">Latest news</a>
-                        <a class="nav-item nav-link" href="#">Albums</a>
-                        <a class="nav-item nav-link" href="#">Fans</a>
+                        <button class="navbar-toggler my_navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="close fas fa-times"></i>
+                        </button>
+                        <div class="navbar-nav my_navbar-nav">
+                            <a class="nav-item nav-link active" href="#">Home</a>
+                            <a class="nav-item nav-link" href="#">Meet the brand</a>
+                            <a class="nav-item nav-link" href="#">Live dates</a>
+                            <a class="nav-item nav-link" href="#">Latest news</a>
+                            <a class="nav-item nav-link" href="#">Albums</a>
+                            <a class="nav-item nav-link" href="#">Fans</a>
                         </div>
                     </div>
                 </nav>
@@ -56,14 +59,19 @@ export default {
 <style lang="scss">
 @import "../assets/scss/style.scss";
 
-.container-fluid {
+.my_fluid {
     background-image: url('../assets/img/home_slider.jpg');
     background-size: cover;
     background-position: 50%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     .navbar-container {
         top: 0;
         left: 0;
         width: 100%;
+        z-index: 999;
         .my_navbar-brand {
             width: 15%;
             img {
@@ -74,7 +82,8 @@ export default {
             background-color: $mandy;
         }
         .navbar-dark {
-            transition: background-color 300ms ease-out;
+            transition: background-color 700ms ease;
+            position: relative;
             .my_navbar-toggler {
                 border: none;
             }
@@ -82,14 +91,39 @@ export default {
                 box-shadow: none;
             }
             .my_collapse {
+                position: absolute;
                 text-align: center;
+                height: 100vh;
+                width: 100%;
+                top: 0;
+                left: 0;
+                background-color: $mandy;
+                .my_navbar-toggler {
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                    margin: 1.1em;
+                }
+                .my_navbar-nav {
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%,-50%);
+                }
+                .nav-item {
+                    font-size: 2em;
+                    font-weight: 300;
+                }
+            }
+            .collapsing {
+                transition: none;
             }
             .close {
                 font-size: 1.6em;
             }
-            button[aria-expanded="true"] > span.navbar-toggler-icon {
-                display: none;
-            }
+            // button[aria-expanded="true"] > span.navbar-toggler-icon {
+            //     display: none;
+            // }
 
             button[aria-expanded="false"] > .close {
                 display: none;
@@ -101,21 +135,22 @@ export default {
         color: $white;
         padding: 12em 0;
         h1 {
-            font-size: 6em;
+            font-size: 8em;
             font-weight: 700;
         }
         .cursive {
             font-family: $cursive;
+            font-size: 1.5em;
         }
         .buttons {
             .my_btn {
                 text-transform: uppercase;
                 background-color: $mandy;
                 color: $white;
-                font-size: 0.8em;
+                font-size: 1.5em;
                 border-radius: 0.1em;
-                padding-left: 2em;
-                padding-right: 2em;
+                padding: 0.7em 2em;
+                font-weight: 600;
             }
             .my_btn:hover {
                 background-color: $white;
