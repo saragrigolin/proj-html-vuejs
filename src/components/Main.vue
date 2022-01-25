e8283c7b1f649517d2d8a7244302683ce61c9703
saragrigolin <sara.grigolin95@gmail.com>
2022-01-25 15:07:40 +0100

    live dates click

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -94,15 +94,95 @@
             </div>
             <div class="container">
                 <ul class="date-list px-0 py-4 w-80">
-                    <li class="date-list-item"><i class="fas fa-plus"></i><span>17/08/2020 gem festival 2020 anakalia, georgia</span></li>
-                    <li class="date-list-item"><i class="fas fa-plus"></i><span>24/09/2020 groovegest dominical republic</span></li>
-                    <li class="date-list-item"><i class="fas fa-plus"></i><span>31/10/2020 oasis festival 2020 marrakech, marocco</span></li>
-                    <li class="date-list-item"><i class="fas fa-plus"></i><span>07/11/2020 moga festival - essaouria, marocco</span></li>
-                    <li class="date-list-item"><i class="fas fa-plus"></i><span>10/12/2020 envision festival - uvita, costa rica</span></li>
+                    <li class="date-list-item" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                        <div class="li-header">
+                            <i class="fas fa-plus"></i><span>17/08/2020 gem festival 2020 anakalia, georgia</span>
+                        </div>
+                        <div class="collapse" id="collapse1">
+                            <div class="details d-flex">
+                                <img class="p-3" src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDsUcTjt43mTheN9ruCsQVgBE-wgN6_AfY&language=en&center=New+York&maptype=terrain&zoom=14&size=370x250&markers=icon:https://avada.theme-fusion.com/music/wp-content/uploads/sites/68/2017/09/custom_marker-1.png|New+York&scale=2" alt="">
+                                <div class="details-content justify-content-end p-3 d-flex flex-column">
+                                    <span class="details-title">
+                                        Untold Stories
+                                    </span>
+                                    <span class="details-text py-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus vero perspiciatis facilis exercitationem voluptates repudiandae at saepe cum cupiditate! Accusamus ex, voluptate hic et debitis ipsa in sequi tenetur tempora?</span>
+                                    <button type="button" class="my_btn btn w-25">book now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="date-list-item" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                        <div class="li-header">
+                            <i class="fas fa-plus"></i><span>24/09/2020 groovegest dominical republic</span>
+                        </div>
+                        <div class="collapse" id="collapse2">
+                            <div class="details d-flex">
+                                <img class="p-3" src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDsUcTjt43mTheN9ruCsQVgBE-wgN6_AfY&language=en&center=New+York&maptype=terrain&zoom=14&size=370x250&markers=icon:https://avada.theme-fusion.com/music/wp-content/uploads/sites/68/2017/09/custom_marker-1.png|New+York&scale=2" alt="">
+                                <div class="details-content justify-content-end p-3 d-flex flex-column">
+                                    <span class="details-title">
+                                        Untold Stories
+                                    </span>
+                                    <span class="details-text py-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus vero perspiciatis facilis exercitationem voluptates repudiandae at saepe cum cupiditate! Accusamus ex, voluptate hic et debitis ipsa in sequi tenetur tempora?</span>
+                                    <button type="button" class="my_btn btn w-25">book now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="date-list-item" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                        <div class="li-header">
+                            <i class="fas fa-plus"></i><span>31/10/2020 oasis festival 2020 marrakech, marocco</span>
+                        </div>
+                        <div class="collapse" id="collapse3">
+                            <div class="details d-flex">
+                                <img class="p-3" src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDsUcTjt43mTheN9ruCsQVgBE-wgN6_AfY&language=en&center=New+York&maptype=terrain&zoom=14&size=370x250&markers=icon:https://avada.theme-fusion.com/music/wp-content/uploads/sites/68/2017/09/custom_marker-1.png|New+York&scale=2" alt="">
+                                <div class="details-content justify-content-end p-3 d-flex flex-column">
+                                    <span class="details-title">
+                                        Untold Stories
+                                    </span>
+                                    <span class="details-text py-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus vero perspiciatis facilis exercitationem voluptates repudiandae at saepe cum cupiditate! Accusamus ex, voluptate hic et debitis ipsa in sequi tenetur tempora?</span>
+                                    <button type="button" class="my_btn btn w-25">book now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="date-list-item" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                        <div class="li-header">
+                            <i class="fas fa-plus"></i><span>07/11/2020 moga festival - essaouria, marocco</span>
+                        </div>
+                        <div class="collapse" id="collapse4">
+                            <div class="details d-flex">
+                                <img class="p-3" src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDsUcTjt43mTheN9ruCsQVgBE-wgN6_AfY&language=en&center=New+York&maptype=terrain&zoom=14&size=370x250&markers=icon:https://avada.theme-fusion.com/music/wp-content/uploads/sites/68/2017/09/custom_marker-1.png|New+York&scale=2" alt="">
+                                <div class="details-content justify-content-end p-3 d-flex flex-column">
+                                    <span class="details-title">
+                                        Untold Stories
+                                    </span>
+                                    <span class="details-text py-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus vero perspiciatis facilis exercitationem voluptates repudiandae at saepe cum cupiditate! Accusamus ex, voluptate hic et debitis ipsa in sequi tenetur tempora?</span>
+                                    <button type="button" class="my_btn btn w-25">book now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="date-list-item" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+                        <div class="li-header">
+                            <i class="fas fa-plus"></i><span>10/12/2020 envision festival - uvita, costa rica</span>
+                        </div>
+                        <div class="collapse" id="collapse5">
+                            <div class="details d-flex">
+                                <img class="p-3" src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDsUcTjt43mTheN9ruCsQVgBE-wgN6_AfY&language=en&center=New+York&maptype=terrain&zoom=14&size=370x250&markers=icon:https://avada.theme-fusion.com/music/wp-content/uploads/sites/68/2017/09/custom_marker-1.png|New+York&scale=2" alt="">
+                                <div class="details-content justify-content-end p-3 d-flex flex-column">
+                                    <span class="details-title">
+                                        Untold Stories
+                                    </span>
+                                    <span class="details-text py-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus vero perspiciatis facilis exercitationem voluptates repudiandae at saepe cum cupiditate! Accusamus ex, voluptate hic et debitis ipsa in sequi tenetur tempora?</span>
+                                    <button type="button" class="my_btn btn w-25">book now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    
                 </ul>
             </div>
         </div>
-
 
     </main>
 </template>
@@ -237,21 +317,35 @@ export default {
         list-style-type: none;
         margin: 0 auto;
         .date-list-item {
-            text-transform: uppercase;
             padding: 1em 1.5em;
             margin-bottom: 0.5em;
-            display: flex;
-            align-items: center;
             background-color: $backgroundMusicIsLife;
             cursor: pointer;
-            span {
-                color: $mandy;
-                margin-left: 0.5em;
-                font-size: 2em;
+            .li-header {
+                text-transform: uppercase;
+                display: flex;
+                align-items: center;
+                span {
+                    color: $mandy;
+                    margin-left: 0.5em;
+                    font-size: 2em;
+                }
+                .fa-plus {
+                    font-size: 1.5em;
+                    padding: 0.2em;
+                }
             }
-            .fa-plus {
-                font-size: 1.5em;
-                padding: 0.2em;
+            .details {
+                img {
+                    width: 30%;
+                }
+                .details-title {
+                    font-size: 1.5em;
+                    font-weight: 300;
+                }
+                .details-text {
+                    color: $text;
+                }
             }
         }
         .date-list-item:hover {
